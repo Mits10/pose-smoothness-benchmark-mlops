60f8441e2e0fe56d1a221a1886204edbbaa4e353
Tanbin Akter Mitaly <38568420+Mits10@users.noreply.github.com>
2026-03-11 23:20:07 +0100

    load of xsens data completed

--- src/io/load_xsens.py
+++ src/io/load_xsens.py
@@ -1,12 +1,95 @@
-import argparse
 import logging
 from pathlib import Path
+import xml.etree.ElementTree as ET
+import pandas as pd
 
 logging.basicConfig(
     level = logging.INFO,
     format = "%(asctime)s|%(levelname)s|%(message)s"
 )
-def process_one_file() -> None:
+
+def split_xyz(values: list[float], names: list[str], prefix: str) -> dict[str, float]: 
+    cols: dict[str, float] = {} 
+    for i, name in enumerate(names): 
+        base = i * 3 
+        if base + 2 >= len(values): 
+            break 
+        cols[f"{name}_{prefix}X"] = values[base] 
+        cols[f"{name}_{prefix}Y"] = values[base + 1] 
+        cols[f"{name}_{prefix}Z"] = values[base + 2] 
+    return cols
+
+def split_quat(values: list[float], names: list[str]) -> dict[str, float]: 
+    cols: dict[str, float] = {} 
+    for i, name in enumerate(names): 
+        base = i * 4 
+        if base + 3 >= len(values): 
+            break 
+        cols[f"{name}_qw"] = values[base] 
+        cols[f"{name}_qx"] = values[base + 1] 
+        cols[f"{name}_qy"] = values[base + 2] 
+        cols[f"{name}_qz"] = values[base + 3] 
+    return cols
+
+def parse_mvnx_file(input_file:Path) -> pd.DataFrame:
+    #parsing the XML
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+
+    #Handling the XML namespace
+    ns = {"m": root.tag.split("}")[0].strip("{")}
+
+    #Reading metadata
+    segments = [s.attrib["label"] for s in root.findall(".//m:segment", ns)]
+    joints = [j.attrib["label"] for j in root.findall(".//m:joint", ns)]
+    sensors = [s.attrib["label"] for s in root.findall(".//m:sensor", ns)]
+
+    #Creating row for storage
+    rows: list[dict[str,float]] = []
+
+    #looping through frame to store segments values
+    for frame in root.findall(".//m:frame", ns):
+        row: dict[str, float] = {}
+        row["time_s"] = float(frame.attrib.get("time", 0.0))
+
+        for child in frame:
+            tag = child.tag.split("}")[-1]
+            if child.text is None or not child.text.strip():
+                continue
+            values = list(map(float, child.text.split()))
+
+            if tag == "position": row.update(split_xyz(values, segments, "")) 
+            elif tag == "velocity": row.update(split_xyz(values, segments, "V")) 
+            elif tag == "acceleration": row.update(split_xyz(values, segments, "A")) 
+            elif tag == "orientation": row.update(split_quat(values, segments)) 
+            elif tag == "sensorAcceleration": row.update(split_xyz(values, sensors, "A")) 
+            elif tag == "sensorOrientation": row.update(split_quat(values, sensors)) 
+            elif tag == "jointAngle": 
+                for i, joint in enumerate(joints): 
+                    if i >= len(values): 
+                        break 
+                    row[f"{joint}_angle"] = values[i] 
+
+        rows.append(row)
+    df = pd.DataFrame(rows)
+    return df
+
+def process_one_file(input_file: Path, output_dir: Path) -> Path:
+    output_dir.mkdir(parents = True, exist_ok=True)
+    output_file = output_dir / f"{input_file.stem}_wide.csv"
+
+    #Checking if the file already processed
+    if output_file.exists():
+        if output_file.stat().st_mtime >= input_file.stat().st_mtime:
+            logging.info("Skipping %s (already processed)", input_file)
+            return output_file
+
+    logging.info("Processing %s",input_file)
+    df = parse_mvnx_file(input_file)
+    df.to_csv(output_file, index=False)
+    logging.info("Saving %s", output_file)
+
+
     return None
 
 def process_directory(input_dir: Path, output_dir: Path, recursive: bool = True) -> None:
@@ -17,7 +100,7 @@ def process_directory(input_dir: Path, output_dir: Path, recursive: bool = True)
         logging.warning("No mvnx files found in %s", input_dir)
         return
    
-    logging.info("Found %d .mvnx files in %s", input_dir)
+    logging.info("Found %d .mvnx files in %s", len(files),input_dir)
 
     for file_path in files:
         try:
@@ -25,30 +108,3 @@ def process_directory(input_dir: Path, output_dir: Path, recursive: bool = True)
         except Exception as exc:
             logging.exception("Failed processing %s : %s", file_path,exc)
 
-def main() -> None:
-
-    #Command line Argument
-    parser = argparse.ArgumentParser(description="Preprocess MVNX files into wide csv tables.")
-    parser.add_argument("--input", required=True, help="Input .mvnx files or directory")
-    parser.add_argument("--output", required=True, help="Output directory")
-    parser.add_argument("--no-recursive", action="store_true", help="Disable recursive directory scan")
-
-    #Reading Argument
-    args = parser.parse_args()
-    input_path = Path(args.input)
-    output_dir = Path(args.output)
-
-    #Validating input exists
-    if not input_path.exists():
-        raise FileNotFoundError(f"Input path does not exist: {input_path}")
-    
-    #Deciding file vs folder behaviour
-    if input_path.is_file():
-        if input_path.suffix.lower() != ".mvnx":
-            raise ValueError(f"Expected a .mvnx file, got: {input_path}")
-        process_one_file(input_path,output_dir)
-    else:
-        process_directory(input_path,output_dir, recursive = not args.no_recursive)
-
-if __name__ == "__main__":
-    main()
---
+++ src/pipelines/prepare_dataset.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import argparse
+from src.io.load_xsens import process_directory,process_one_file
+
+def main() -> None:
+
+    #Command line Argument
+    parser = argparse.ArgumentParser(description="Preprocess MVNX files into wide csv tables.")
+    parser.add_argument("--input", required=True, help="Input .mvnx files or directory")
+    parser.add_argument("--output", required=True, help="Output directory")
+    parser.add_argument("--no-recursive", action="store_true", help="Disable recursive directory scan")
+
+    #Reading Argument
+    args = parser.parse_args()
+    input_path = Path(args.input)
+    output_dir = Path(args.output)
+
+    #Validating input exists
+    if not input_path.exists():
+        raise FileNotFoundError(f"Input path does not exist: {input_path}")
+    
+    #Deciding file vs folder behaviour
+    if input_path.is_file():
+        if input_path.suffix.lower() != ".mvnx":
+            raise ValueError(f"Expected a .mvnx file, got: {input_path}")
+        process_one_file(input_path,output_dir)
+    else:
+        process_directory(input_path,output_dir, recursive = not args.no_recursive)
+
+if __name__ == "__main__":
+    main()
