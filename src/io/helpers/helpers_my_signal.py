344e969734e74374bc3770ba0acf2132ed258065
Tanbin Akter Mitaly <38568420+Mits10@users.noreply.github.com>
2026-03-16 17:28:00 +0100

    filters_visualization added

--- src/io/helpers/helpers_my_signal.py
+++ src/io/helpers/helpers_my_signal.py
@@ -1,2 +1,16 @@
+import numpy as np
+from pathlib import Path
+
 from src.io.load_xsens import load_xsens_pose_sequence
 from src.preprocessing.filters import lowpass_filter
+
+def get_signal(path: Path) -> np.array:
+    data = load_xsens_pose_sequence(
+                path,
+                sequence_id="xsens_csv_trial",
+                subject_id="59",
+                task="hand_motion",
+                trial=2210,
+                kind="position",
+    ).joints["left_hand"][:25]
+    return data
